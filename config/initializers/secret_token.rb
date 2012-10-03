e2cc5f7744a69baa6f9f4bbfc208a7b21bab7f23
Ruben Nodeland <ruben.nodeland@gmail.com>
2012-10-03 18:18:50 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeedEnging::Application.config.secret_token = 'c90d39d75be3f149671583dfa8575ce4ee0955d9b8b2ae96a3fac528d29dc863e33fee4b46deca71cc28c3bce868a7b5ddfdea9fa467521e3a5b3f167bfa961f'
